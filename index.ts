3b172f8f1251b9bb27590dcd1048b86b1ac61166
Konstantin <shuvalov.2021@list.ru>
2022-06-26 14:55:27 +0300

    fix: replace websocket server in entrypoint file

--- index.ts
+++ index.ts
@@ -3,7 +3,18 @@ import { wss } from './src/ws_server/index';
 
 const HTTP_PORT = 3000;
 
+wss.on('connection', ws => {
+
+  ws.on('message', data => {
+    console.log(data.toString());
+  });
+
+  ws.send('something');
+});
+
+
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
+
 httpServer.listen(HTTP_PORT);
 
 
